e91e51f1af857efd374a3c36bd0f8f8b70df9069
reed-foster <reed.a.foster@gmail.com>
2023-09-24 22:01:13 -0400

    a few more bugs to get rid of in 3d case

--- divergence_matrix_3d.m
+++ divergence_matrix_3d.m
@@ -3,5 +3,5 @@ function div = divergence_matrix_3d(L,M,N)
   % the matrix transforms a 1D state vector ordered first by increasing spatial coordinate along the x,y,z axes (respectively),
   % then by vector direction e.g. V_x(0,0,0) V_x(1,0,0) ... V_x(0,1,0) ... V_y(0,0,0) ...
   % L is the number of points in space in the x direction, M the number of points in the y direction, and N in the z direction
-  div = [kron(eye(N*M),divergence_matrix_1d(L)) kron(kron(eye(N),divergence_matrix_1d(M),eye(L)) kron(diag(divergence_matrix_1d(N),eye(M*L)))];
+  div = [kron(eye(N*M),divergence_matrix_1d(L)) kron(kron(eye(N),divergence_matrix_1d(M)),eye(L)) kron(divergence_matrix_1d(N),eye(M*L))];
 end
