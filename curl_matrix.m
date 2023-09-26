cb1200ea33de9ec5ac0852c89b2769862e35e31f
Reed Foster <reed.a.foster@gmail.com>
2023-09-26 12:11:56 -0400

    updated docstrings

--- curl_matrix.m
+++ curl_matrix.m
@@ -1,6 +1,5 @@
 function C = curl_matrix(L,M,N)
   % returns the central finite difference approximation of the curl of a vector field in R3
-  % the matrix transform
   % the matrix transforms a 1D state vector ordered first by increasing spatial coordinate along the x,y,z axes (respectively),
   % then by vector direction e.g. V_x(0,0,0) V_x(1,0,0) ... V_x(0,1,0) ... V_y(0,0,0) ...
   % L is the number of points in space in the x direction, M the number of points in the y direction, and N in the z direction
--- curl_matrix_sparse.m
+++ curl_matrix_sparse.m
@@ -1,5 +1,9 @@
 function C = curl_matrix_sparse(L,M,N)
-  %
+  % computes the curl matrix using sparse matrices
+  % returns the central finite difference approximation of the curl of a vector field in R3
+  % the matrix transforms a 1D state vector ordered first by increasing spatial coordinate along the x,y,z axes (respectively),
+  % then by vector direction e.g. V_x(0,0,0) V_x(1,0,0) ... V_x(0,1,0) ... V_y(0,0,0) ...
+  % L is the number of points in space in the x direction, M the number of points in the y direction, and N in the z direction
   curl_matrix = [0 -1 1; 1 0 -1; -1 1 0];
   P = L*M*N;
   ddx = kron(speye(N*M),divergence_matrix_1d(L));
