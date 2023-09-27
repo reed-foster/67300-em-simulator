34330d2adc18b3465e6e351dda979b02ea683ecc
reed-foster <reed.a.foster@gmail.com>
2023-09-26 23:56:53 -0400

    rewrote to use parameters

--- OneDimProblem.m
+++ OneDimProblem.m
@@ -1,28 +1,32 @@
-function dxdt = OneDimProblem(H, E, E_dt, P, P_dt, J, N,  delta_x, eps_0, mu_0, chi_1, chi_2, omega_chi_1, delta_chi_1)
-    N_equation = 5; 
-    dxdt = zeros(N_equation*N, 1);
-    for i = 1:N
+function dxdt = OneDimProblem(X,J,p)
+    dxdt = zeros(5*p.N, 1);
+    for i = 1:p.N
         % dHdt <- dXdt(1:N)
+        % dEdt = dXdt(p.N+1:2*p.N)
+        % dE'dt = dXdt(2*p.N+1:3*p.N)
+        % dPdt -> dXdt(3*p.N+1:4*p.N)
+        % dP'dt -> dXdt(4*p.N+1:5*p.N)
+        % eqn 1
         if i == 1
-            dxdt(i) = 1/mu_0*(E(i+1))/(2*delta_x);
-        elseif i == N
-            dxdt(i) = 1/mu_0*(-E(i-1))/(2*delta_x);
+            dxdt(i) = 1/p.mu_0*(X(i+1+p.N))/(2*p.delta_x);
+        elseif i == p.N
+            dxdt(i) = 1/p.mu_0*(-X(i-1+p.N))/(2*p.delta_x);
         else
-            dxdt(i) = 1/mu_0*(E(i+1)-E(i-1))/(2*delta_x);
+            dxdt(i) = 1/p.mu_0*(X(i+1+p.N)-X(i-1+p.N))/(2*p.delta_x);
         end
-        % dEdt = dXdt(N+1:2*N)
-        dxdt(i+N) = E_dt(i); 
-        % dE'dt = dXdt(2*N+1:3*N)
+        % eqn 2
+        dxdt(i+p.N) = X(i+2*p.N);
+        % eqn 3
         if i == 1 
-           dxdt(i+2*N) = -(H(i+1))/(2*delta_x) + eps_0*E_dt(i)+P_dt(i) + 2*eps_0*chi_2*E(i)*E_dt(i)+J(i);
-        elseif i == N 
-           dxdt(i+2*N) = (H(i-1))/(2*delta_x) + eps_0*E_dt(i)+P_dt(i) + 2*eps_0*chi_2*E(i)*E_dt(i)+J(i);
+           dxdt(i+2*p.N) = -(X(i+1))/(2*p.delta_x) + p.eps_0*X(i+2*p.N)+X(i+4*p.N) + 2*p.eps_0*p.chi_2*X(i+p.N)*X(i+2*p.N)+J(i);
+        elseif i == p.N 
+           dxdt(i+2*p.N) = (X(i-1))/(2*p.delta_x) + p.eps_0*X(i+2*p.N)+X(i+4*p.N) + 2*p.eps_0*p.chi_2*X(i+p.N)*X(i+2*p.N)+J(i);
         else
-        dxdt(i+2*N) = -(H(i+1)-H(i-1))/(2*delta_x) + eps_0*E_dt(i)+P_dt(i) + 2*eps_0*chi_2*E(i)*E_dt(i)+J(i);
+            dxdt(i+2*p.N) = -(X(i+1)-X(i-1))/(2*p.delta_x) + p.eps_0*X(i+2*p.N)+X(i+4*p.N) + 2*p.eps_0*p.chi_2*X(i+p.N)*X(i+2*p.N)+J(i);
         end
-        % dPdt -> dXdt(3*N+1:4*N)
-        dxdt(i+3*N) = P_dt(i);
-        % dP'dt -> dXdt(4*N+1:5*N)
-        dxdt(i+4*N) = eps_0*omega_chi_1^2*chi_1*E(i)-omega_chi_1^2*P(i)-delta_chi_1*P_dt(i);
+        % eqn 4
+        dxdt(i+3*p.N) = X(i+4*p.N);
+        % eqn 5
+        dxdt(i+4*p.N) = p.eps_0*p.omega_chi_1^2*p.chi_1*X(i+p.N)-p.omega_chi_1^2*X(i+3*p.N)-p.delta_chi_1*X(i+4*p.N);
     end
 end
