35116d51fb5f5ccad0c51c9d679e261043a2882b
reed-foster <reed.a.foster@gmail.com>
2023-09-27 01:57:20 -0400

    freespace evalf and ode45 solver works

--- evalf_freespace.m
+++ evalf_freespace.m
@@ -3,7 +3,7 @@ function dxdt = evalf_freespace(X,J,p)
     for i = 1:p.N
         % dHdt <- dXdt(1:N)
         % dEdt = dXdt(p.N+1:2*p.N)
-        % eqn 1
+        % eqn 1: d_t H_y = 1/mu_0*(d_x E_z)
         if i == 1
             dxdt(i) = 1/p.mu_0*(X(i+1+p.N))/(2*p.delta_x);
         elseif i == p.N
@@ -11,13 +11,13 @@ function dxdt = evalf_freespace(X,J,p)
         else
             dxdt(i) = 1/p.mu_0*(X(i+1+p.N)-X(i-1+p.N))/(2*p.delta_x);
         end
-        % eqn 2
-        if i == 1 
-           dxdt(i+p.N) = 1/p.eps_0*((X(i+1))/(2*p.delta_x) + J(i));
-        elseif i == p.N 
-           dxdt(i+p.N) = 1/p.eps_0*(-(X(i-1))/(2*p.delta_x) + J(i));
+        % eqn 2: d_t E_z = 1/eps_0*(d_x H_y - J_z)
+        if i == 1
+            dxdt(i+p.N) = 1/p.eps_0*((X(i+1))/(2*p.delta_x) - J(i));
+        elseif i == p.N
+            dxdt(i+p.N) = 1/p.eps_0*(-(X(i-1))/(2*p.delta_x) - J(i));
         else
-            dxdt(i+p.N) = 1/p.eps_0*((X(i+1)-X(i-1))/(2*p.delta_x) + J(i));
+            dxdt(i+p.N) = 1/p.eps_0*((X(i+1)-X(i-1))/(2*p.delta_x) - J(i));
         end
     end
 end
--- evalf_freespace_test.m
+++ evalf_freespace_test.m
@@ -6,7 +6,7 @@ close all
 p.N = 1500; % discretization
 p.eps_0 =  8.85e-12; % F/m
 p.mu_0 = 1.26e-6; % N/A^2
-p.delta_x = 5e-9; % m
+p.delta_x = 100e-9; % m
 
 % nodal variables
 E = zeros(p.N,1); % electric field 
@@ -18,13 +18,13 @@ J = zeros(p.N,1);
 % simulation setup and initial conditions
 x = linspace(0,(p.N-1)*p.delta_x,p.N);
 
-M = eye(2*p.N);
+M = kron([1 0; 0 1], eye(p.N));
 
-X0 = [sqrt(p.eps_0/p.mu_0)*gaussian_start(1,100,400,300,p.N)'; -sqrt(p.mu_0/p.eps_0)*gaussian_start(1,100,400,300,p.N)']; % V/m and A/m
+X0 = [sqrt(p.eps_0/p.mu_0)*gaussian_start(1,100,400,100,p.N)'; -gaussian_start(1,100,400,100,p.N)']; % V/m and A/m
 
 Xp0 = evalf_freespace(X0,J,p);
 
-figure(11);
+figure(2);
 subplot(2,1,1);
 yyaxis left;
 plot(x*1e6, X0(1:p.N));
@@ -38,25 +38,38 @@ yyaxis right;
 plot(x*1e6, Xp0(p.N+1:2*p.N));
 legend("d_tH_y(x,0)", "d_tE_z(x,0)");
 
-%return;
+tspan = linspace(0,100e-15,1001); % s
 
-tspan = linspace(0,1e-12,10000); % s
+options = odeset('Mass',M,'MassSingular','no','RelTol',1e-3,'AbsTol',1e-6);
 
-options = odeset('Mass',M,'MassSingular','yes','RelTol',1e-4,'AbsTol',1e-8);
-
-[t,X] = ode15s(@(t,X) evalf_freespace(X,J,p), tspan, X0, options);
+[t,X] = ode45(@(t,X) evalf_freespace(X,J,p), tspan, X0, options);
 
 H_t = X(:,1:p.N);
 E_t = X(:,p.N+1:2*p.N);
 
-for i=1:10
-  figure(i);
-  plot(x*1e6, E_t(1000*(i-1)+1,:), '-o');
-  hold on;
-  plot(x*1e6, H_t(1000*(i-1)+1,:), '-o');
+dt = 1;
+figure(1);
+
+video = VideoWriter('evalf_freespace_test.avi'); %Create a video object
+open(video); % Open video source - restricts the use of video for your program
+
+for i=1:size(tspan,2)
+  yyaxis left;
+  plot(x*1e6, E_t(dt*(i-1)+1,:), '-o');
+  ylabel("field [V/m]");
+  ylim([-2 2]);
+  yyaxis right;
+  plot(x*1e6, 1e3*H_t(dt*(i-1)+1,:), '-o');
+  ylim([-10 10]);
+  ylabel("field [mA/m]");
   
   legend("E_z(x,0)", "H_y(x,0)");
   xlabel("x [um]");
-  ylabel("field [A/m or V/m]");
-  title(sprintf("t = %d [ps]", tspan(1000*(i-1)+1)*1e12));
+  title(sprintf("t = %d [ps]", tspan(dt*(i-1)+1)*1e12));
+  drawnow;
+  vidFrame = getframe(gcf);
+  clf;
+  writeVideo(video,vidFrame);
 end
+
+close(video);
