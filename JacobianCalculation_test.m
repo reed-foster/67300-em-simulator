f4e7c7a5a53d9e21b0c72a366a848090a18c3d0f
abuzzimit <abuzzi@mit.edu>
2023-09-27 15:01:49 -0400

    Jacobian calculation test

---
+++ JacobianCalculation_test.m
@@ -0,0 +1,45 @@
+clear
+close all
+clc
+
+%parameters 
+p.N = 4; % discretization
+p.eps_0 =  8.85e-12; % F/m
+p.mu_0 = 1.26e-6; % N/A^2
+p.delta_x = 100e-9; % m
+
+% nodal variables
+E = zeros(p.N,1); % electric field 
+H = zeros(p.N,1); % magnetic field 
+
+% source 
+J = zeros(p.N,1);
+
+% simulation setup and initial conditions
+X0 = [sqrt(p.eps_0/p.mu_0)*gaussian_start(1,100,400,100,p.N)'; -gaussian_start(1,100,400,100,p.N)']; % V/m and A/m
+
+% change logarithmicly the step
+eps = logspace(10, -20, 10);
+rel_err = zeros(length(eps), 1);
+
+% analytical jacobian
+TL = zeros(p.N, p.N);
+TR = diag(ones(p.N-1, 1).*(1/(p.mu_0*2*p.delta_x)), 1) - diag(ones(p.N-1, 1).*(1/(p.mu_0*2*p.delta_x)), -1);
+BL = diag(ones(p.N-1, 1).*(1/(p.eps_0*2*p.delta_x)), 1) - diag(ones(p.N-1, 1).*(1/(p.eps_0*2*p.delta_x)), -1);
+BR = zeros(p.N, p.N);
+
+JacobAnalytic = [TL, TR; BL, BR];
+
+% compute Jacobian
+for i = 1:length(eps)
+    Jcalc = JacobianCalculation(@(X) evalf_freespace(X,J,p), X0, eps(i), 2*p.N);
+    rel_err(i) = norm(Jcalc - JacobAnalytic)/norm(JacobAnalytic);
+end
+
+loglog(eps, rel_err, 'LineWidth', 1.5)
+title('Relative error for Jacobian over $\epsilon$', 'Interpreter', 'latex')
+xlabel('$\epsilon$', 'Interpreter', 'latex')
+ylabel('$\frac{\|Jc - Ja\|}{\|Ja\|}$', 'Interpreter', 'latex')
+grid on
+
+
---
+++ JacobianCalculation_test_trivial.m
@@ -0,0 +1,17 @@
+clear
+close all
+clc
+
+A = 1;
+B = 10;
+C = 3;
+D = 2;
+E = 4;
+F = 5;
+
+f = @(x) [A*x(1) + B*x(2) + x(3); C*x(1) + D*x(2) + x(3); E*x(1) + F*x(2) + x(3)];
+
+eps = 0.001;
+
+Start = [f([1, 0, 0]), f([0, 1, 0]), f([0, 0, 1])]
+Jacob = JacobianCalculation(@(x) f(x), [1, 1, 1], eps, 3)
