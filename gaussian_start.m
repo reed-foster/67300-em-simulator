9802bd3c625750408b4a95713f105f928583ccf6
abuzzimit <abuzzi@mit.edu>
2023-09-26 22:50:24 -0400

    Update gaussian_start.m

--- gaussian_start.m
+++ gaussian_start.m
@@ -4,7 +4,7 @@ function gauss_wavelet = gaussian_start(ampl, width, pos, sp_wave, N)
     % pos = mean of the gaussian
     % sp_wave = wavelength of the sinusoidal modulation
     % N = number of points
-    f = @(x, mu, std) ampl.*exp(-(x-mu).^2./(2*std.^2)).*sin(2*pi*x/sp_wave);
+    f = @(x, mu, std) ampl.*exp(-(x-mu).^2./(2*std.^2)).*cos(2*pi*(x-pos)/sp_wave);
     x = 1:N;
     gauss_wavelet = f(x, pos, width);
 end
