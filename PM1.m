d36d8754fe1b3022ddac356f99261d8ff3e27952
reed-foster <reed.a.foster@gmail.com>
2023-09-27 00:05:42 -0400

    update PM1.m to use parameters

--- PM1.m
+++ PM1.m
@@ -2,48 +2,57 @@ clear all
 close all
 %clc
 
-% nodal variables
-N = 500; % discretization
-E = zeros(N,1); % electric field 
-E_dt = zeros(N,1); % first derivative of E
-H = zeros(N,1); % magnetic field 
-P = zeros(N,1); % polarization vector
-P_dt = zeros(N,1); % first derivative of P
-
 %parameters 
-eps_0 =  8.85e-12; % F/m
-mu_0 = 1.26e-6; % N/A^2
-omega_chi_1 = 2*pi*2.9e15; % Hz
-chi_1 = 4;
-chi_2 = 41.7e-12; % m/V %Laboratory for Nanoscale Optics, John A. Paulson School of Engineering and Applied Sciences, Harvard University
-delta_chi_1 = 2*pi*1.2e13; % Hz
-delta_x = 50e-9; % m
+p.N = 1000; % discretization
+p.eps_0 =  8.85e-12; % F/m
+p.mu_0 = 1.26e-6; % N/A^2
+p.omega_chi_1 = 2*pi*2.9e15; % Hz
+p.chi_1 = 4;
+p.chi_2 = 41.7e-12; % m/V %Laboratory for Nanoscale Optics, John A. Paulson School of Engineering and Applied Sciences, Harvard University
+p.delta_chi_1 = 2*pi*1.2e13; % Hz
+p.delta_x = 5e-9; % m
+
+% nodal variables
+E = zeros(p.N,1); % electric field 
+E_dt = zeros(p.N,1); % first derivative of E
+H = zeros(p.N,1); % magnetic field 
+P = zeros(p.N,1); % polarization vector
+P_dt = zeros(p.N,1); % first derivative of P
 
 % source 
-J = zeros(N,1);
+J = zeros(p.N,1);
 
 % simulation setup and initial conditions
-M = diag([ones(2*N, 1); zeros(N, 1); ones(2*N, 1)]);
-options = odeset('Mass',M,'RelTol',1e-4,'AbsTol',1e-8);
+M = diag([ones(2*p.N, 1); zeros(p.N, 1); ones(2*p.N, 1)]);
 
-X0 = [zeros(N,1) zeros(N,1) zeros(N,1) zeros(N,1) zeros(N,1)]; % V/m
-tspan = linspace(0,100e-15,50); % s
+X0 = [1e-9*gaussian_start(1,100,400,500,p.N)'; 1e-9*gaussian_start(1,100,400,500,p.N)'; zeros(p.N,1); zeros(p.N,1); zeros(p.N,1)]; % V/m or A/m
+Xp0 = OneDimProblem(X0,J,p);
+X0(2*p.N+1:3*p.N) = Xp0(p.N+1:2*p.N);
+X0(3*p.N+1:4*p.N) = p.eps
+X0(4*p.N+1:5*p.N) = Xp0(3*p.N+1:4*p.N);
+%X0([1 N N+1 2*N+1]) = 0;
+%X0 = [zeros(N,1) zeros(N,1) zeros(N,1) zeros(N,1) zeros(N,1)]; % V/m
+tspan = linspace(0,1e-12,200); % s
 
-[t,X] = ode15s(@(t,X) f(t,X,J,N,delta_x,eps_0,mu_0,chi_1,chi_2,omega_chi_1,delta_chi_1),tspan,X0,options);
+%implicitDAE = @(Xp) M*Xp - M*OneDimProblem(X0,J,p);
+%Xp = fsolve(implicitDAE, X0);
 
-H_t = X(:,1:N);
-E_t = X(:,N+1:2*N);
+options = odeset('Mass',M,'MassSingular','yes','RelTol',1e-4,'AbsTol',1e-8);
 
-x = linspace(0,(N-1)*delta_x,N);
-plot(x*1e6, E_t(10,:), '-o');
-hold on;
-plot(x*1e6, H_t(10,:), '-o');
+[t,X] = ode15s(@(t,X) OneDimProblem(X,J,p), tspan, X0, options);
 
-legend("E_z(x,0)", "H_y(x,0)");
-xlabel("x [um]");
-ylabel("field [A/m or V/m]");
+H_t = X(:,1:p.N);
+E_t = X(:,p.N+1:2*p.N);
 
-
-function out = f(t,X,J,N,delta_x,eps_0,mu_0,chi_1,chi_2,omega_chi_1,delta_chi_1)
-  out = OneDimProblem(X(1:N),X(N+1:2*N),X(2*N+1:3*N),X(3*N+1:4*N),X(4*N+1:5*N),J,N,delta_x,eps_0,mu_0,chi_1,chi_2,omega_chi_1,delta_chi_1);
+x = linspace(0,(p.N-1)*delta_x,p.N);
+for i=1:10
+  figure(i);
+  plot(x*1e6, E_t(20*(i-1)+1,:), '-o');
+  hold on;
+  plot(x*1e6, H_t(20*(i-1)+1,:), '-o');
+  
+  legend("E_z(x,0)", "H_y(x,0)");
+  xlabel("x [um]");
+  ylabel("field [A/m or V/m]");
+  title(sprintf("t = %d [fs]", tspan(i)));
 end
