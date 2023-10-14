3d504d9c07ba7070a3e4c384424a63c94ee9d0f1
reed-foster <reed.a.foster@gmail.com>
2023-10-14 13:24:49 -0400

    finished writing first version of 1D nonlinear wave equation

--- evalf_nonlinear_wave_1D.m
+++ evalf_nonlinear_wave_1D.m
@@ -23,20 +23,32 @@ function dxdt = evalf_nonlinear_wave_1D(X,dtJ,p)
   % get the offset into the state variable
   E = X(1:idx_step:end); % Nx1
   dtE = X(2:idx_step:end); % Nx1
-  P = zeros(p.N, num_poles); % NxP
-  dtP = zeros(p.N, num_poles); % NxP
-  for i = 1:p.N
-    for p = 1:p.P
-      P(i,p) = X(3+idx_step*(i-1)+2*(p-1));
-      dtP(i,p) = X(3+idx_step*(i-1)+2*(p-1)+1);
-    end
+  P = zeros(num_poles, p.N); % PxN
+  dtP = zeros(num_poles, p.N); % PxN
+  for p = 1:num_poles
+    P(p) = X(3+2*(p-1):idx_step:end);
+    dtP(p) = X(4+2*(p-1):idx_step:end);
   end
 
-  for i = 1:p.N
-    % eqn 1: rank reduction
-    dxdt(i*idx_step+E_idx_offset) = dtE;
-    % eqn 2: wave equation
-    dxdt(i*idx_step+dtE_idx_offset) = (1/p.mu_0*laplacian_1D(i,idx_step,E_idx_offset,X,p.N,p.dz) ...
-                                        - 2*p.eps_0*(p.chi_2 + 3*p.chi_3*X(;
+  % e.g. with 3 poles
+  % X = [E_1, dtE_1, P_11, dtP_11, P_21, dtP_21, P_31, dtP_31, E_2, dtE_2, P_12, dtP_12 ...]
+  %      1    2      3     4       5     6       7     8       9    10     11    12
+  % idx_step = 2*(1 + 3) = 8;
+
+  % eqn 1: rank reduction (d/dt(E) = dtE);
+  dxdt(1:idx_step:end) = dtE;
+  dtdtP = zeros(num_poles, p.N);
+  for p = 1:num_poles
+    % eqn 3: rank reduction (d/dt(P) = dtP);
+    dxdt(3+2*(p-1):idx_step:end) = dtP(p);
+    % eqn 4: dispersive polarization response (d/dt(dtP) = omega^2*eps0*delta_chi*E - omega^2*P - delta*dtP)
+    dtdtP(p) = (p.P(p,3)^2*p.eps_0*p.P(p,1)).*E - (p.P(p,3)^2).*P(p) - (p.P(p,2)).*dtP(p);
+    dxdt(4+2*(p-1):idx_step:end) = dtdtP(p);
   end
+  % eqn 2: wave equation (d/dt(dtE) = 1/mu_0*(d/dz)(d/dz)E ...);
+  dxdt(2:idx_step:end) = (1/p.mu_0*del2(E,dz^2) ...
+                          - (2*p.eps_0).*(p.chi_2 + E.*(3*p.chi_3)).*(dtE.^2) ...
+                          - sum(dtdtP, 1) ...
+                          + dtJ) ./ (1 + (2*p.chi_2).*E + (3*p.chi_3).*(E.^2));
+  return dxdt;
 end
