c17828570cd4a0566ede60c97ecb94844c054f13
reed-foster <reed.a.foster@gmail.com>
2023-11-25 17:02:17 -0500

    removed some unused old code

--- test.m
+++ test.m
@@ -3,18 +3,12 @@ close all
 
 p = params();
 
-% source 
-p.tsteps = round(p.tf/p.dt);
-p.tvec = linspace(0,(p.tsteps-1)*p.dt,p.tsteps); % s
-
 % simulation setup and initial conditions
 E0 = zeros(p.N,1);
 dtE0 = zeros(p.N, 1);
 P0 = zeros(size(p.Lorentz,1), p.N);
 dtP0 = zeros(size(p.Lorentz,1), p.N);
 X0 = generate_X(E0, dtE0, P0, dtP0, p);
-
-options = odeset('RelTol',1e-3,'AbsTol',1e-12);
 
 newton_opts.err_f = Inf;
 newton_opts.err_dx = 1e-8;
