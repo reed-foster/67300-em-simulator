2ab286ed017b5c7c2dff6716b610c12e39041962
abuzzimit <abuzzi@mit.edu>
2023-11-29 16:13:03 -0500

    Fourier and test

---
+++ field_fft.m
@@ -0,0 +1,34 @@
+function field_fft(E, z, t, p)
+    color_order = lines(2);
+    x = E;
+    subplot(2, 1, 1);
+    plot(z, x, 'linewidth', 2, 'Color', color_order(1, :));
+    xlabel('Position (m)');
+    ylabel('Amplitude');
+    title("Signal in Time Domain at t = " + num2str(t) + " s")
+
+    % Fourier transform
+    subplot(2, 1, 2);
+    Ts = p.dz;
+    y = fft(x);
+    fs = 1 / Ts;    
+    n = length(x);
+    fshift = (-n/2:n/2-1) * (fs/n);
+    yshift = fftshift(y);
+    % semilogy(fshift, abs(yshift), 'linewidth', 2, 'Color', color_order(2, :));
+    plot(fshift, abs(yshift), 'linewidth', 2, 'Color', color_order(2, :));
+    title("Signal in Wavevector Domain")
+    xlabel('Wavevector (1/m)');
+    ylabel('Amplitude');
+    xlim([-max(fshift)/4, max(fshift)/4]);
+    % ylim([0, max(max(abs(fft(E))))])
+    %ylim([min(min(abs(fft(E)))), max(max(abs(fft(E))))])
+    % ylim([1e-6, 1e2]);
+    
+    % % Aggiungi una pausa tra i frame
+    % pause(0.001);
+    % 
+    % frame = getframe(gcf);
+    % M(k) = frame;
+    % k = k+1;
+end
---
+++ movie_fft.m
@@ -0,0 +1,39 @@
+function movie_fft(X, t, p, movie)
+
+    % Change when vectorize split_X
+    for i = 1:size(X, 2)
+        [E(:, i), ~, ~, ~] = split_X(X(:, i), p);
+    end
+    
+    % Change it save z somewhere
+    z_temp = 0:p.dz:(p.dz*size(E, 1)-p.dz);
+    z = z_temp - mean(z_temp);
+    
+    figure;
+    k = 1;
+    for i = 1:100:length(E(1, :))
+        
+        field_fft(E(:, i), z, t(i), p)
+        pause(0.1)
+    
+        frame = getframe(gcf);
+        M(k) = frame;
+        k = k+1;
+    end
+
+    if movie == true
+        Prova = M(floor(1:0.3:end));
+    
+        % Create a VideoWriter object
+        writerObj = VideoWriter('TestMovie.mp4', 'MPEG-4');
+        open(writerObj);
+        
+        % Write frames to the video file
+        for i = 1:length(Prova)
+            writeVideo(writerObj, Prova(i).cdata);
+        end
+        
+        % Close the video file
+        close(writerObj);
+    end
+end
