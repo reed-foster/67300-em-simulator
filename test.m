58d0de60f4d143776d9c359095cabf2037534e66
Reed Foster <reed.a.foster@gmail.com>
2023-11-29 18:52:38 -0500

    tried to do vectorized GCR, didn't work because of numerical instability

--- test.m
+++ test.m
@@ -22,7 +22,7 @@ newton_opts.err_gcr = 1e-2; % relative error for GCR residual inside Newton
 newton_opts.eps_fd = 1e-7; % relative perturbation for Jacobian
 
 trap_opts.save_intermediate = false;
-trap_opts.visualize = Inf;
+trap_opts.visualize = 10;
 
 tic;
 [t,X] = trapezoid(@eval_f, p, @eval_u, X0, p.tf, p.dt, trap_opts, newton_opts);
