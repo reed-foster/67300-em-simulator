3032acd563632e8eb2e895ebf12f49fa3845e75f
reed-foster <reed.a.foster@gmail.com>
2023-12-01 15:33:06 -0500

    added dynamical timestepping to trapezoidal, seems to be worse, but there may be a way to optimize it

--- trapezoid.m
+++ trapezoid.m
@@ -1,4 +1,4 @@
-function [t,X,max_k] = trapezoid(eval_f,p,u,x0,tf,dt,trap_opts,newton_opts);
+function [t,X] = trapezoid(eval_f,p,u,x0,tf,dt_max,trap_opts,newton_opts);
    % trapezoidal integrator to simulate state evolution of model dx/dt=f(x,p,u)
    %
    % usage:
@@ -9,10 +9,12 @@ function [t,X,max_k] = trapezoid(eval_f,p,u,x0,tf,dt,trap_opts,newton_opts);
    % u: handle to input function u(t, p)
    % x0: initial state
    % tf: simulation stop time
-   % dt: timestep
+   % dt_max: maximum timestep. If adaptive timestepping is disabled,
+   %   this is used as a fixed timestep
    % trap_opts: struct with options for trapezoidal integrator
-   %  visualize: if Inf, don't visualize, otherwise, show every 'visualize'th frame
+   %  visualize_dt: if Inf, don't visualize, otherwise, show every data every visualize_dt step
    %  save_intermediate: if true, save all results, otherwise only save final result
+   %  adaptive_timestep: if true, increase the timestep adaptively without exceeding dt
    % newton_opts: struct with options for Newton:
    %  err_f: termination condition for error on |f(x)|
    %  err_dx: termination condition on |dx|
@@ -21,35 +23,50 @@ function [t,X,max_k] = trapezoid(eval_f,p,u,x0,tf,dt,trap_opts,newton_opts);
    X(:,1) = x0;
    max_k = 1;
 
-   if (trap_opts.visualize < Inf)
+   if (trap_opts.visualize_dt < Inf)
       visualize_struct.init = true;
       visualize_struct = visualize_state(X(:,1), t(1), p, visualize_struct);
    end
 
-   for l = 1:ceil(tf/dt)
-      if trap_opts.save_intermediate
-         lcurr = l;
-         lnext = l+1;
-      else
-         lcurr = 1;
-         lnext = 1;
-      end
-      dt_l = min(dt, tf-t(lcurr));
-      u_t = u(t(lcurr), p);
+   %for l = 1:ceil(tf/dt)
+   if (trap_opts.adaptive_timestep)
+      dt = dt_max/10;
+   else
+      dt = dt_max;
+   end
+   t_last_visualized = 0;
+   while t(end) < tf
+      dt_l = min(dt, tf-t(end));
+      u_t = u(t(end), p);
       % part of f_trap that doesn't depend on x, so precompute it to save time
-      gamma = X(:,lcurr) + dt_l/2*eval_f(X(:,lcurr),p,u_t);
+      gamma = X(:,end) + dt_l/2*eval_f(X(:,end),p,u_t);
       % trap function and jacobian
       f_trap = @(x,p,u) x - dt_l/2*eval_f(x,p,u) - gamma;
       % call newton to solve f_trap
-      [x,converged,err_f_k,err_dx_k,err_rel_k,k,~] = newton(f_trap, p, u_t, X(:,lcurr) + dt_l*eval_f(X(:,lcurr),p,u_t), newton_opts);
-      if k > max_k
-         max_k = k;
-      end
-      if mod(l, trap_opts.visualize) == 0
-         visualize_state(X(:,lcurr),t(lcurr),p,visualize_struct);
+      [x,converged,err_f_k,err_dx_k,err_rel_k,k,k_gcr,~] = newton(f_trap, p, u_t, X(:,end) + dt_l*eval_f(X(:,end),p,u_t), newton_opts);
+      % update X
+      if trap_opts.save_intermediate
+         X(:,end+1) = x;
+         t(end+1) = t(end) + dt_l;
+      else
+         X(:,end) = x;
+         t(end) = t(end) + dt_l;
       end
       % use number of iterations to dynamically adjust timestep
-      X(:,lnext) = x;
-      t(lnext) = t(lcurr) + dt_l;
+      if (trap_opts.adaptive_timestep)
+         if (k_gcr > 40)
+            dt = dt/5;
+         elseif (k_gcr > 25)
+            dt = dt/2;
+         elseif (k_gcr < 20)
+            dt = min(dt*1.5,dt_max);
+         end
+      end
+      disp(num2str([k_gcr dt], "GCR took %d iterations, setting timestep to %d"));
+      % visualize
+      if t_last_visualized + trap_opts.visualize_dt < t(end)
+         visualize_state(X(:,end),t(end),p,visualize_struct);
+         t_last_visualized = t(end);
+      end
    end
 end
